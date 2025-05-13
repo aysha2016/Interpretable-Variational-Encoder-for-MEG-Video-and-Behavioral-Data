05915281f8abf8ea0470782280743734581bdce2
Aysha munawwara   Rafik <amunawwara@gmail.com>
2025-05-13 06:30:52 +0530

    Delete Interpretable Variational Encoder (IVE) for MEG, Video, and Behavioral Data directory

--- Interpretable Variational Encoder (IVE) for MEG, Video, and Behavioral Data/data_preprocessing.py
+++
@@ -1,9 +0,0 @@
-import mne
-import numpy as np
-
-def preprocess_meg_data(fif_file):
-    raw = mne.io.read_raw_fif(fifload_file, preload=True)
-    raw.filter(1., 40., fir_design='firwin')
-    events = mne.find_events(raw)
-    epochs = mne.Epochs(raw, events, event_id=None, tmin=-0.2, tmax=0.5, baseline=(None, 0))
-    return epochs.get_data()
